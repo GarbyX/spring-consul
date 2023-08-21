2a7de62bb2da94b22fb085406c8dc5d918a1771c
Gabriel Madichie <amadichie@yahoo.com>
2023-08-21 15:45:46 +0100

    Spring Consul Initial commit

---
+++ src/main/java/com/garby/springconsul/SpringConsulApplication.java
@@ -0,0 +1,13 @@
+package com.garby.springconsul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConsulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConsulApplication.class, args);
+	}
+
+}
